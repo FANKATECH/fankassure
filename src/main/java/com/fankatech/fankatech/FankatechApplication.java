b7be3703506ba48e525919a39f95296f3def0e0d
Mohamed TOURE <medchelios@gmail.com>
2025-04-14 20:14:41 -0400

    first commit

---
+++ src/main/java/com/fankatech/fankatech/FankatechApplication.java
@@ -0,0 +1,13 @@
+package com.fankatech.fankatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FankatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FankatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fankatech/fankatech/FankatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fankatech.fankatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FankatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
